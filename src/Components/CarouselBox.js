c935b5905734f2694061da42eb4356182c3102f9
VovaGrab <114055237+VovaGrab@users.noreply.github.com>
2024-06-18 15:10:32 +0300

    Add files via upload

---
+++ src/Components/CarouselBox.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import Carousel from 'react-bootstrap/Carousel'
+import con1Img from '../assets/con1.jpg';
+import con2Img from '../assets/con2.png';
+import con3Img from '../assets/con3.jpg';
+import con4Img from '../assets/con4.webp';
+import con5Img from '../assets/con5.webp';
+import con6Img from '../assets/con6.png';
+import { CarouselCaption } from 'react-bootstrap';
+
+export default class CarouselBox extends Component {
+  render() {
+    return (
+      <Carousel>
+        <Carousel.Item>
+            <img
+                className='d-block w-100'
+                src={con1Img}
+                alt='Contests'
+            />
+            <Carousel.Caption>
+                <h3>WELCOME TO BOOLK !</h3>
+                <p>"Boolk: Elevate Your Talent, Embrace the Challenge!"</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+
+        
+      </Carousel>
+    )
+  }
+}
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import PageTitle from '../Components/PageTitle';
-
+import CarouselBox from '../Components/CarouselBox';
+import Carousel from 'react-bootstrap/Carousel'
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <PageTitle title="Home" />
-        Home Page Content
-      </div>
+      <CarouselBox/>
     );
   }
 }
